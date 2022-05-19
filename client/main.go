1212c1d7fcb2ba767dbfd0f96aa2869dc7d1f411
ansel brandt <ansel@anselbrandt.com>
2022-05-19 02:52:15 -0400

    Update Command

--- client/main.go
+++ client/main.go
@@ -75,7 +75,7 @@ func main() {
 		fmt.Printf("Connection State has changed %s \n", connectionState.String())
 
 		if connectionState == webrtc.ICEConnectionStateConnected {
-			out, err := exec.Command("pwd").Output()
+			out, err := exec.Command("libcamera-vid", "-t", "0", "-o", "-|", "ffmpeg", "-framerate", "30", "-i", "-", "-c:v", "libx264", "-g", "10", "-preset", "ultrafast", "-tune", "zerolatency", "-f", "rtp", "'rtp://127.0.0.1:5004?pkt_size=1200'").Output()
 			if err != nil {
 				log.Fatal(err)
 			}
